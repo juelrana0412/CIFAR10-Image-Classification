3568e85491bda8c71c4b34bd8d941e1f32b42044
Juel-Rana <md.juelrana0412@gmail.com>
2026-07-01 22:12:22 +0600

    Add Docker deployment

--- app.py
+++ app.py
@@ -88,9 +88,9 @@ def classify():
 
     )
 
-
 if __name__ == "__main__":
-
     app.run(
-        debug=True
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 7860)),
+        debug=False
     )
